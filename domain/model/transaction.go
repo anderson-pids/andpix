75bcc3a40def1d514d6dce1b4759d91d2d5bf2ab
Anderson Pimentel <apds.anderson@gmail.com>
2022-04-05 22:30:53 -0400

    feat: Create transaction model

---
+++ domain/model/transaction.go
@@ -0,0 +1,73 @@
+package model
+
+import (
+	"errors"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+const (
+	TransactionPending   string = "pending"
+	TransactionCompleted string = "completed"
+	TransactionError     string = "error"
+	TransactionConfirmed string = "confirmed"
+)
+
+type Transactions struct {
+	transaction []Transaction
+}
+
+type Transaction struct {
+	Base              `valid:"required"`
+	AccountFrom       *Account `valid:"-"`
+	Amount            float64  `json:"amount" valid:"notnull"`
+	PixKeyTo          *PixKey  `valid:"-"`
+	Status            string   `json:"status" valid:"notnull"`
+	Description       string   `json:"description" valid:"notnull"`
+	CancelDescription string   `json:"cancel_description" valid:"-"`
+}
+
+func (t *Transaction) isValid() error {
+	_, err := govalidator.ValidateStruct(t)
+
+	if t.Amount <= 0 {
+		return errors.New("the amount must be greater than 0")
+	}
+
+	if t.Status != TransactionPending && t.Status != TransactionCompleted && t.Status != TransactionError {
+		return errors.New("invalid status for the transaction")
+	}
+
+	if t.PixKeyTo.AccoundId == t.AccountFrom.ID {
+		return errors.New("the source and destination account cannot be the same")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewTransaction(accountFrom *Account, amount float64, pixKeyTo *PixKey, description string) (*Transaction, error) {
+	transaction := Transaction{
+		AccountFrom: accountFrom,
+		Amount:      amount,
+		PixKeyTo:    pixKeyTo,
+		Description: description,
+		Status:      TransactionPending,
+	}
+
+	transaction.ID = uuid.NewV4().String()
+	transaction.CreatedAt = time.Now()
+
+	err := transaction.isValid()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &transaction, nil
+}
